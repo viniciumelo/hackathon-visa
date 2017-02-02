ec678f71fb3dd138559636b5ac8f172f36b8024a
Day <dayannefernandesc@gmail.com>
2017-02-02 04:26:41 -0200

    init mod packbuy

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,22 +1,17 @@
 <hr>
-<section id="Helloworld8" data-speed="6" data-type="background">
-    	<div class="row" id="contato">
-		<div class="col-md-12">
-	
-</div>
 
 	</section>
     <div class="row" >
 		<div class="col-md-12">
       <!-- Footer -->
     <footer>
-    
+
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    
+
                     <p class="copyright text-muted" align="center">Copyright &copy; 2017 Packbuy.</p>
-                    
+
                 </div>
             </div>
         </div>
@@ -25,10 +20,9 @@
 	</div>
 </div>
 
-
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/scripts.js"></script>
+
   </body>
 </html>
-
--- index.php
+++ index.php
@@ -17,77 +17,65 @@
 <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
     <title>Packbuy</title>
 
-   
+
     <meta name="Vinicius Melo" content="Packbuy">
-    
+
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 	<link href="css/customCss.css" rel="stylesheet">
-	
-	    <script src="css/bootstrap.min.js"></script>
-	<script type="text/javascript">
 
-      $(document).ready(function(){
-         $window = $(window);
-         $('section[data-type="background"]').each(function(){
-           var $scroll = $(this);                 
-            $(window).scroll(function() {
-              var yPos = -($window.scrollTop() / $scroll.data('speed')); 
-               var coords = '50% '+ yPos + 'px';
-              $scroll.css({ backgroundPosition: coords });    
-            });
-         });  
-      }); 
-      </script>
-
-
+      <script type="text/javascript">
+         function onVisaCheckoutReady(){
+         V.init( {
+         apikey: "YRIBX37BHIIJNRNXE9A821LbBgZOZU9WtBv7Vtz2-rzLMx5vk",
+          paymentRequest:{
+           currencyCode: "USD",
+           total: "10.00"
+         }
+         });
+       V.on("payment.success", function(payment)
+         {alert(JSON.stringify(payment)); });
+       V.on("payment.cancel", function(payment)
+         {alert(JSON.stringify(payment)); });
+       V.on("payment.error", function(payment, error)
+         {alert(JSON.stringify(error)); });
+       }
+       </script>
 
   </head>
   <body>
 
     <div class="container-fluid">
-	<div class="row">
-    
-		<div class="col-md-12">
-			
-			<br>
-			<br>
-			<br>
-			<div align="right">
-			
-											<a href="login.php">entrar</a>  | 
-						
-						
-							<a href="cadastro.php">Cadastrar-se</a>
+	     <div class="row">
+
+		       <div class="col-md-12">
+
+		           <div align="right">
+
+	                <a href="login.php">entrar</a>  |
+
+	                <a href="cadastro.php">Cadastrar-se</a>
 							</div>
-			
-                <img class="logo-topo" id="logo" alt="Packbuy"src="img/logo.png" class="img-default">
-				
-				
-				
-		</div>
-		
-	</div>
-   <br>
-   <br>
-   
-<?php 
+
+              <img class="logo-topo" id="logo" alt="Packbuy"src="img/logo.png" class="img-default">
+          </div>
+
+     </div>
+
+    <!-- <img alt="Visa Checkout" class="v-button" role="button" src="https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png"/> -->
+
+   <!-- <script type="text/javascript"
+   src="https://sandbox-assets.secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js">
+   </script> -->
+
+<?php
 require_once 'system/config.php';
 require_once 'system/database.php';
 ?>
 
 <div class="row" id="packbuy>
-    
-
-        
-        
-
-
-			
-					
-                
 
             <?php
 
@@ -97,41 +85,37 @@ require_once 'system/database.php';
             if( !$posts ){
                 echo '<h2>Nenhuma produto encontrado!</h2>';
            } else{
-			   
+
                 foreach ( $posts as $post ):
-                    
+
 					$categ = DBRead( 'categoria', "WHERE id = '". $post['categoria'] ."'" );
                 $categ = ( $categ ) ? $categ[0]['titulo'] : 'SEM CATEGORIA';
                 ?>
-				
+
 		<div class="col-md-4" >
 					<div class="thumbnail" >
-                
-                    <a href="produtos.php?id=<?php echo $post['id']; ?>" title="<?php echo $post['modelo']; ?>">
-					<img src="<?php echo $post['foto']; ?>" alt="<?php echo $post['modelo']; ?>">
-					<div class="caption">
-							<h3>
-								<?php echo $post['modelo']; ?>
-							</h3>
-							</a>
-							<p align="center">
-								R$:  <?php echo $post['preco']; ?>, 00
-							</p>
-							
-                    </a>
-                            
 
-                
-                
-				</div>
-				</div>
-</div>
+                      <a href="produtos.php?id=<?php echo $post['id']; ?>" title="<?php echo $post['modelo']; ?>">
+  					<img src="<?php echo $post['foto']; ?>" alt="<?php echo $post['modelo']; ?>">
+  					<div class="caption">
+  							<h3>
+  								<?php echo $post['modelo']; ?>
+  							</h3>
+  							</a>
+  							<p align="center">
+  								R$:  <?php echo $post['preco']; ?>, 00
+  							</p>
 
-            <?php endforeach; 
+                      </a>
+  				</div>
+				</div>
+      </div>
+
+            <?php endforeach;
                 }
             ?>
 
-</div>
+  </div>
 </div>
 </div>
     <?php include'includes/rodape.php';?>
